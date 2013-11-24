d735c9fc242a4997fe3940bbb45579c6de23225b
ppx80 <fmichelucci@gmail.com>
2013-11-24 23:32:39 +0100

    serve a ststic files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 //NodeJS Server
-var express = require('express'),
+var express = require('express');
 	fs = require('fs');
 
 var app = express();
 app.use(express.static(__dirname + "/../client"));
 
-app.get('/',function(req, res){
+/*app.get('/',function(req, res){
 	res.end('Ciao Mondo');
-});
+});*/
 
 app.listen(8080);
