29bf0ecc4c5bf35c9bf11e422ff2577e84fb563c
ppx80 <fmichelucci@gmail.com>
2013-11-25 14:45:25 +0100

    add socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 //NodeJS Server
-var express = require('express');
-	fs = require('fs');
+var express = require('express')
+	, fs = require('fs')
+	, io = require('socket.io')
 
 var app = express();
+var srv = express.createServer();
+
 app.use(express.static(__dirname + "/../client"));
 
 /*app.get('/',function(req, res){
 	res.end('Ciao Mondo');
 });*/
+
+//app.get('/v1/events')
 
 app.listen(8080);
